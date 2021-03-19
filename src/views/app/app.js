9618610bab299e4d95e489294f0d0ff3edeb511f
Jono Cosio <jonoluiz@gmail.com>
2021-03-19 20:42:38 +0800

    Fix breadth search

--- src/views/app/app.js
+++ src/views/app/app.js
@@ -1,41 +1,52 @@
 const { ipcRenderer } = require("electron");
 const pup = require("puppeteer");
 
-function getUrl(){
-    var urlBox = document.getElementById('urlInput');
-    return urlBox.value;
+function getUrl() {
+	var urlBox = document.getElementById("urlInput");
+	return urlBox.value;
 }
 
-function addToList(href){
-    var list = document.getElementById('linkList');
-    var item = document.createElement('li');
-    item.setAttribute('class', 'list-group-item');
-    item.innerHTML = href;
+function addToList(href) {
+	var list = document.getElementById("linkList");
+	var item = document.createElement("li");
+	item.setAttribute("class", "list-group-item");
+	item.innerHTML = href;
 
-    list.appendChild(item);
+	list.appendChild(item);
 }
 
-ipcRenderer.on('update-visited-links', (event, arg) => {
-    if(arg){
-        console.log("NOT EMPTY LIST");
-    }
+function clearList() {
+    var list = document.getElementById("linkList");
+    list.innerHTML = "";
+}
+
+ipcRenderer.on("update-visited-links", (event, arg) => {
+	if (arg) {
+		console.log("NOT EMPTY LIST");
+	}
 });
 
 async function initiate() {
-    var url = getUrl();
+    // Clear current list
+    clearList();
+
+	var url = getUrl();
+
 	// Launch Browser in non-visual mode
 	const browser = await pup.launch({
 		headless: true,
 	});
 	console.log("Launched Browser");
+
 	// Rewrite url to proper format for browsing
 	if (!url.includes("http")) {
 		url = "https://" + url;
 	}
 	// Instatiate Arrays
 	var sitemap = [];
-	var visited = [];
 	var currPageInSiteMap = 0;
+
+    // Set domain and initial search target;
 	var baseUrl = new URL(url);
 	baseUrl = baseUrl.href;
 
@@ -43,41 +54,49 @@ async function initiate() {
 
 	// Check valid links and visit all
 	const extractAllLinks = async (u) => {
-		if (visited.includes(u)) {
+		if (currPageInSiteMap == sitemap.length) {
+            console.log("Reached end of sitemap");
 			return;
 		} else {
 			const tab = await browser.newPage();
 			console.log("Opening: ", u);
 			await tab.goto(u, { waitUntil: "load", timeout: 0 });
-			await tab.waitForTimeout(3000);
+			await tab.waitForTimeout(5000);
 
-            const currentPage = await tab.url();
+			const currentPage = await tab.url();
 
 			const getLinks = await tab.evaluate((base) => {
 				var links = document.querySelectorAll("a");
-                console.log(links);
+				console.log(links);
 				var pageHrefs = [];
 				links.forEach((link) => {
 					const ripped = link.getAttribute("href");
 					var nonRelativeUrl = new URL(ripped, base).href;
-					if (nonRelativeUrl.includes(new URL(base).hostname) && (!nonRelativeUrl.includes('#') || !nonRelativeUrl.includes('null'))) {
+					if (
+						nonRelativeUrl.includes(new URL(base).hostname) &&
+						(!nonRelativeUrl.includes("#") &&
+							!nonRelativeUrl.includes("null"))
+					) {
 						pageHrefs.push(nonRelativeUrl);
 					}
 				});
 				return pageHrefs;
 			}, currentPage);
 
-            visited.push(currentPage);
-            addToList(currentPage);
+			// visited.push(currentPage);
+			addToList(u);
 
 			getLinks.forEach((link) => {
-                if (!sitemap.includes(link)) {
-                    sitemap.push(link);
-                }
+				if (!sitemap.includes(link)) {
+					sitemap.push(link);
+				}
 			});
-			++currPageInSiteMap;
+			currPageInSiteMap++;
 			tab.close();
-            console.log(currPageInSiteMap);
+            // console.log(`Current Page: ${u}`);
+			// console.log(`Sitemap Length: ${sitemap.length}`);
+            // console.log(`Index of Current Page: ${sitemap.indexOf(u)}`);
+            // console.log(`Next Index: ${currPageInSiteMap}`);
 			await extractAllLinks(sitemap[currPageInSiteMap]);
 		}
 	};
@@ -85,9 +104,9 @@ async function initiate() {
 	// Call for actual crawling
 	console.log(`Extracting all links of ${baseUrl}`);
 	await extractAllLinks(baseUrl);
-    console.log("Extraction End");
+	console.log("Extraction End");
 	// Close browser instance
 	await browser.close();
 
-    ipcRenderer.sendSync('console-display', sitemap);
+	ipcRenderer.sendSync("console-display", sitemap);
 }
