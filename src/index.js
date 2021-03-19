be2a1d369c00422df2285b3b211dc17708aac452
Jono Cosio <jonoluiz@gmail.com>
2021-03-19 17:30:47 +0800

    Remove reference

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
-const { initiate } = require('./crawler');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
